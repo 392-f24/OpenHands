cab997748f0f630131c52971445716d4273fcbd2
WeiViv <100058931+WeiViv@users.noreply.github.com>
2024-11-18 13:08:00 -0600

    fix(schema type): fixed the type restrictions for the two types of users, and seperate the type definition for organizations into the user.d.ts file
    1. move the type definition of organization users into the user.d.ts
    2. rename the type definition file for donation schedule info as donation.d.ts
    3. incorporate the idea of a basicprofile interface and extend the interfaces for the two type of users from the basic profile

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,25 +1,41 @@
-interface UserContextType {
-  user: User | undefined;
-  loading: boolean;
-  login: (userType: UserType, navigate: NavigateFunction) => Promise<void>;
-  logout: (navigate: NavigateFunction) => Promise<void>;
-  updateProfile: (updates: Partial<User>) => Promise<void>;
-}
-
 type UserType = 'donor' | 'organization';
 
-interface DonorProfile {
+interface BasicProfile {
   uid: string;
   name: string;
+  email: string;
   profilePic: string;
   createdAt: Date;
-  role: 'donor' | 'organization';
+  role: UserType;
 }
 
-interface OrganizationProfile extends DonorProfile {
+interface DonorProfile extends BasicProfile {
+  joinedEvents: string[]; // array of eventIds
+  providedSupplies: {
+    eventId: string;
+    itemName: string;
+    quantityProvided: number;
+    organizationId: string;
+  }[];
+}
+
+interface OrganizationProfile extends BasicProfile {
   location: string;
   description: string;
   website: string;
+  events: {
+    eventId: string;
+    title: string;
+    description: string;
+    date: Date;
+    supplies: {
+      itemName: string;
+      quantityNeeded: number;
+      quantityProvided: number;
+      providedBy: string[]; // array of uid of donors
+      status: 'needed' | 'fulfilled';
+    }[];
+  }[];
 }
 
 type User = OrganizationProfile | DonorProfile;
