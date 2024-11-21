bc8eaf6757b0b02cc5bd2a5dee0c7e2b8b97cc7d
MC2b6 <micaserio606@gmail.com>
2024-11-20 22:21:11 -0600

    feat: fetch organizations from firestore + update saved to firestore

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { Box, Typography } from '@mui/material';
 import { filter, lowerCase, some } from 'es-toolkit/compat';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { useUser } from '@/hooks';
 
