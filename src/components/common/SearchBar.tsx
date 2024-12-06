1aefcf1fe80882a8ff8ea3c78d69dd13b3a1c086
WelldoneM <welldonematanga12@gmail.com>
2024-12-06 10:46:36 -0600

    changes to search bar

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,34 +1,4 @@
-// import { TextField, Box} from '@mui/material';
-
-// interface SearchBarProps {
-//   searchQuery: string;
-//   setSearchQuery: (query: string) => void;
-// }
-
-// const SearchBar: React.FC<SearchBarProps> = ({
-//   searchQuery,
-//   setSearchQuery,
-// }) => (
-//   <Box
-//     display='flex'
-//     alignItems='center'
-//     mt={3}
-//     mx={2}
-//   >
-//     <TextField
-//       label='Search organizations...'
-//       variant='outlined'
-//       fullWidth
-//       sx={{ marginRight: 1 }}
-//       value={searchQuery}
-//       onChange={(e) => setSearchQuery(e.target.value)}
-//     />
-//   </Box>
-// );
-
-// export default SearchBar;
-
-import { TextField, Box } from '@mui/material';
+import { TextField } from '@mui/material';
 
 interface SearchBarProps {
   searchQuery: string;
