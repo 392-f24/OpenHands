805e3f22d56e1192efe57a142c86fd7a21b4fdef
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-12-06 12:00:46 -0600

    fix: fetching org profiles on mount

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -32,13 +32,18 @@ const useUserStore = create<UserState>()(
           async (firebaseUser) => {
             if (firebaseUser) {
               const currentUser = get().user;
-              if (currentUser) {
-                await fetchEventsByIds(currentUser.joinedEvents);
+
+              if (currentUser && currentUser.joinedEvents?.length > 0) {
+                try {
+                  await fetchEventsByIds(currentUser.joinedEvents);
+                } catch (error) {
+                  console.error('Error fetching events:', error);
+                }
               }
+              set({ user: currentUser || undefined });
             } else {
               set({ user: undefined });
-              const eventStore = useEventStore.getState();
-              eventStore.setEvents([]);
+              useEventStore.getState().setEvents([]);
             }
             set({ loading: false });
           }
@@ -69,8 +74,6 @@ const useUserStore = create<UserState>()(
         try {
           set({ loading: true });
           await logoutUser(navigate);
-          const eventStore = useEventStore.getState();
-          eventStore.setEvents([]);
 
           set({ user: undefined, error: null });
         } catch (error) {
